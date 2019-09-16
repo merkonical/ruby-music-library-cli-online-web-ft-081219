437a6c031593834319ac3626668bc39d6db2ecc6
Maverick Adolfo <merkonical@gmail.com>
2019-09-16 07:27:08 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable 
   def find_by_name(name)                  #is added as a class method to classes that extend the module isn't hard-coded
-    self.all.detect{|s| s.name == name}
+    self.all.detect{|s| s.name == name}   #searching the extended class's @@all variable for an instance that matches the provided name
   end
 
   def find_or_create_by_name(name)
