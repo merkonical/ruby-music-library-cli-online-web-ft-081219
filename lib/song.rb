bbeb7a30c248542001a59b20d699469a99e77c5a
Maverick Adolfo <merkonical@gmail.com>
2019-09-16 06:12:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
+    find_by_name(name) || create(name)  #finds name of song/ if not found creates name 
   end 
 end 
 
